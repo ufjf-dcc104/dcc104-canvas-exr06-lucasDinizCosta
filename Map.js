2f3a1510db8d8bde19832c98289468d52b987061
lucasDinizCosta <lucas_diniz_costa@hotmail.com>
2018-06-17 21:55:27 -0300

    Movimentação ajustada

---
+++ Map.js
@@ -0,0 +1,71 @@
+function Map(w,h,s){
+  this.w = w;
+  this.h = h;
+  this.s = s;
+  this.cell = [];
+  for (var l = 0; l < h; l++) {
+    this.cell[l] = [];
+    for (var c = 0; c < w; c++) {
+      this.cell[l][c] = 0;
+    }
+  }
+}
+
+Map.prototype.desenhar = function (ctx) {
+  for (var l = 0; l < this.h; l++) {
+    for (var c = 0; c < this.w; c++) {
+      if(this.cell[l][c] === 0){                  //Vazio
+        ctx.fillStyle = "black";
+        //ctx.strokeStyle = "grey";
+        ctx.fillRect(c*this.s, l*this.s, this.s, this.s);
+        //ctx.strokeRect(c*this.s, l*this.s, this.s, this.s);
+      } else if(this.cell[l][c] === 1){          //Bloqueado
+        ctx.fillStyle = "green";
+        ctx.strokeStyle = "grey";
+        ctx.fillRect(c*this.s, l*this.s, this.s, this.s);
+        ctx.strokeRect(c*this.s, l*this.s, this.s, this.s);
+      } else if(this.cell[l][c] === 2){           //Terreno a desbravar
+        ctx.fillStyle = "lightBlue";
+        ctx.strokeStyle = "grey";
+        ctx.fillRect(c*this.s, l*this.s, this.s, this.s);
+        ctx.strokeRect(c*this.s, l*this.s, this.s, this.s);
+      } else if(this.cell[l][c] === 3){           //Terreno a desbravar com mina embaixo
+        ctx.fillStyle = "lightBlue";
+        ctx.strokeStyle = "grey";
+        ctx.fillRect(c*this.s, l*this.s, this.s, this.s);
+        ctx.strokeRect(c*this.s, l*this.s, this.s, this.s);
+      } else if(this.cell[l][c] === 4){           //Terreno a desbravar com tesouro embaixo
+        ctx.fillStyle = "lightBlue";
+        ctx.strokeStyle = "grey";
+        ctx.fillRect(c*this.s, l*this.s, this.s, this.s);
+        ctx.strokeRect(c*this.s, l*this.s, this.s, this.s);
+      }
+    }
+  }
+};
+
+/*var MAPA1 = [
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+  [1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1],
+  [1,1,1,1,1,1,1,1,0,1,0,0,0,1,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1],
+  [1,0,1,1,1,0,0,0,0,1,0,0,0,1,0,0,0,1,1],
+  [1,0,0,0,0,0,0,0,1,1,1,0,0,1,1,1,0,1,1],
+  [1,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,1],
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+];*/
+
+var MAPA1 = [
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+  [1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1],
+  [1,1,1,1,1,1,1,1,0,1,0,0,0,1,0,0,0,0,1],
+  [1,0,2,2,2,0,0,0,0,0,0,0,0,1,1,1,1,0,1],
+  [1,0,2,2,2,0,0,0,0,0,0,0,0,1,0,0,0,0,1],
+  [1,0,1,1,1,0,0,0,0,1,0,0,0,1,0,0,0,1,1],
+  [1,0,0,0,0,0,0,0,1,1,1,0,0,1,1,1,0,1,1],
+  [1,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,1],
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+];
---
+++ player.js
@@ -0,0 +1,17 @@
+function Player() {
+  this.sprite = new Sprite();
+
+  //Mapa das teclas pressionadas
+  this.up = false;
+  this.down = false;
+  this.right = false;
+  this.left = false;
+}
+
+Player.prototype.mover = function(dt){
+  this.sprite.mover(dt);
+}
+
+Player.prototype.desenhar = function(ctx){
+  this.sprite.desenhar(ctx);
+}
